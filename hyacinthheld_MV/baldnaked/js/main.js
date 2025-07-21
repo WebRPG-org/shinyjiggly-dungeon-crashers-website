7c51ec168c1cc547e4e3bcdd51e67907a83fa463
shinyjiggly <agenttat@gmail.com>
2025-07-20 23:57:24 -0700

    found a fix maybe
    from here: https://forums.rpgmakerweb.com/index.php?threads/save-title.54107/page-2
    thank you kilgore trout

--- hyacinthheld_MV/baldnaked/js/main.js
+++ hyacinthheld_MV/baldnaked/js/main.js
@@ -4,6 +4,19 @@
 
 PluginManager.setup($plugins);
 
+DataManager.isThisGameFile = function(savefileId) 
+{
+	var globalInfo = this.loadGlobalInfo();
+	if (globalInfo && globalInfo[savefileId]) 
+	{
+	return true;
+	} 
+	else 
+	{
+	return false;
+	}
+};
+
 window.onload = function() {
     SceneManager.run(Scene_Boot);
 };
